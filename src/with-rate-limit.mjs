e51cdbf8a85efdf28e15ed0c9ae1f825baebdb6f
George Czabania <1140167+stayradiated@users.noreply.github.com>
2022-11-17 12:45:36 +0100

    feat: log history period

--- src/with-rate-limit.mjs
+++ src/with-rate-limit.mjs
@@ -7,13 +7,22 @@ const createDeferred = () => {
 };
 
 const logHistory = (history) => {
+  const oldestEntry = [...history].sort()[0];
+  const timeSinceOldestEntryMs = oldestEntry ? Date.now() - oldestEntry : 0;
+  const historyPeriod = Math.round(timeSinceOldestEntryMs / 1000);
+
   const size = history.size;
+
+  const avgMs = size ? Math.round(timeSinceOldestEntryMs / size) : 0;
+
   console.log(
     [
       "".padStart(10),
-      "".padStart(6),
-      "".padStart(4),
-      c.green(`${size} requests in this period`),
+      "".padStart(8),
+      "".padStart(7),
+      c.green(
+        `${size} requests in ${historyPeriod} seconds. Average request time is ${avgMs}ms`
+      ),
     ].join(c.gray(", "))
   );
 };
@@ -31,13 +40,15 @@ const withRateLimit = ({ numberOfRequests, periodMs }, fn) => {
   };
 
   const execute = async (...args) => {
-    history.add(args);
     logHistory(history);
+
+    const now = Date.now();
+    history.add(now);
 
     const result = await fn(...args);
 
     setTimeout(() => {
-      history.delete(args);
+      history.delete(now);
       checkQueue();
     }, periodMs);
 
