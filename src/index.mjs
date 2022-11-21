00631dcfd43f958a1eeb9c4e2f321781ffa28947
George Czabania <1140167+stayradiated@users.noreply.github.com>
2022-11-21 12:21:13 +0100

    feat: run 5 requests in parallel

--- src/index.mjs
+++ src/index.mjs
@@ -2,7 +2,7 @@ import { fetch } from "./fetch.mjs";
 import { withCounter } from "./with-counter.mjs";
 import { withRateLimit } from "./with-rate-limit.mjs";
 
-const RATE_LIMIT_REQUESTS = 100;
+const RATE_LIMIT_REQUESTS = 120;
 const RATE_LIMIT_PERIOD = 60 * 1000;
 
 const request = withRateLimit(
@@ -13,8 +13,15 @@ const request = withRateLimit(
 const main = async () => {
   let i = 1;
   while (true) {
-    await request(i);
-    i += 1;
+    // make 5 requests in parallel
+    await Promise.all([
+      request(i),
+      request(i + 1),
+      request(i + 2),
+      request(i + 3),
+      request(i + 4),
+    ]),
+    i += 5;
   }
 };
 
